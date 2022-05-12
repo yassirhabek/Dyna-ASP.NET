f1bbb26b609f712378b7b682d1bdac88b7d81d83
Yassir <yhabek@gmail.com>
2022-05-12 19:15:44 +0200

    asp user implementation

---
+++ ASP/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLogicLayer.Containers;
+using BusinessLogicLayer.Models;
+using DataAccesLayer;
+using Microsoft.AspNetCore.Mvc;
+using TicketApplication.Models;
+
+namespace TicketApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserViewModel userModel)
+        {
+            List<string> errors = new List<string>();
+            User user = new User(userModel.Email, userModel.Password);
+            UserContainer userContainer = new UserContainer(new UserDAL());
+
+            if (userContainer.Login(user, out errors))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(errors);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserViewModel userModel)
+        {
+            List<string> errors = new List<string>();
+            User user = new User(userModel.Email, userModel.UserName, userModel.Password, userModel.VerPw);
+            UserContainer userContainer = new UserContainer(new UserDAL());
+
+            if (userContainer.Register(user, out errors) == 1)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(errors);
+            }
+        }
+
+    }
+}
---
+++ ASP/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketApplication.Models
+{
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string VerPw { get; set; }
+    }
+}
